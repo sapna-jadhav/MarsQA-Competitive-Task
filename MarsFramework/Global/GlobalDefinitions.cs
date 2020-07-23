4c272e871f9389d1928f835f55819a2fd1fc3459
sapna-jadhav <42228469+sapna-jadhav@users.noreply.github.com>
2020-07-23 16:28:41 +1000

    first commit

---
+++ MarsFramework/Global/GlobalDefinitions.cs
@@ -0,0 +1,80 @@
+﻿using Excel;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MarsFramework.Global
+{
+    class GlobalDefinitions
+    {
+        //Initialise the browser
+        public static IWebDriver driver { get; set; }
+
+		//public void Initialize()
+		//{
+		//	//Defining the browser
+		//	driver = new ChromeDriver();
+		//	Wait();
+
+		//	//Maximise the window
+		//	driver.Manage().Window.Maximize();
+		//}
+		#region WaitforElement 
+
+		public static void Wait()
+        {
+           // driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(time);
+			driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+		}
+        public static IWebElement WaitForElement(IWebDriver driver, By by, int timeOutinSeconds)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeOutinSeconds));
+#pragma warning disable CS0618 // Type or member is obsolete
+			return (wait.Until(ExpectedConditions.ElementIsVisible(by)));
+#pragma warning restore CS0618 // Type or member is obsolete
+		}
+		#endregion
+		public static string BaseUrl
+		{
+			get { return ConstantHelpers.Url; }
+		}
+		public static void NavigateUrl()
+		{
+			driver.Navigate().GoToUrl(BaseUrl);
+		}
+
+		
+
+        #region screenshots
+        public class SaveScreenShotClass
+        {
+            public static string SaveScreenshot(IWebDriver driver, string ScreenShotFileName) // Definition
+            {
+                var folderLocation = (Base.ScreenshotPath);
+
+                if (!System.IO.Directory.Exists(folderLocation))
+                {
+                    System.IO.Directory.CreateDirectory(folderLocation);
+                }
+
+                var screenShot = ((ITakesScreenshot)driver).GetScreenshot();
+                var fileName = new StringBuilder(folderLocation);
+
+                fileName.Append(ScreenShotFileName);
+                fileName.Append(DateTime.Now.ToString("_dd-mm-yyyy_mss"));
+                //fileName.Append(DateTime.Now.ToString("dd-mm-yyyym_ss"));
+                fileName.Append(".jpeg");
+                screenShot.SaveAsFile(fileName.ToString(), ScreenshotImageFormat.Jpeg);
+                return fileName.ToString();
+            }
+        }
+        #endregion
+    }
+}
---
+++ MarsFramework/Test/Program.cs
@@ -0,0 +1,37 @@
+﻿using MarsFramework.Global;
+using MarsFramework.Pages;
+using NUnit.Framework;
+using System;
+
+namespace MarsFramework
+{
+    public class Program
+    {
+        [TestFixture]
+        [Category("Sprint1")]
+        class User : Global.Base
+        {
+
+            [Test]
+            public void Test()
+            {
+				//Inititalize();
+			
+				//SignIn Login = new SignIn();
+				//Login.LoginSteps();
+				ShareSkill obj = new ShareSkill();
+				obj.EnterShareSkill();
+				ManageListings List= new ManageListings();
+				List.Listings();
+				ManageListings delete = new ManageListings();
+				delete.DeleteListing();
+				
+		
+
+			}
+
+
+
+        }
+    }
+}
